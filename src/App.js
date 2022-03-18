b1918f331b6838ffa2d983aba6ccd141345cea41
CrackerJaps <thecrackerjaps@gmail.com>
2022-03-17 19:25:17 -0700

    fix some styling - add duplicate movie alert popup

--- src/App.js
+++ src/App.js
@@ -4,56 +4,54 @@ import SearchPage from './components/SearchPage.js'
 import Watchlist from './components/Watchlist.js'
 
 function App() {
+  const [alert, setAlert] = useState(false)
   const [darkMode, setDarkMode] = useState(true)
   const [showWatchlist, setShowWatchlist] = useState(false)
   const [myWatchlist, setMyWatchlist] = useState([])
 
+  // ***** BACKGROUND COLOR - CONDITIONAL ON DARKMODE *****
   const bgStyle = {
     backgroundColor: darkMode ? 'var(--bg-dark)' : 'var(--bg-light)',
     color: darkMode ? 'var(--text-dm)' : 'var(--text-lm)',
   }
 
-  function reorderUp(id) {
-    const currentMovie = myWatchlist.filter((movie) => movie.cardId === id)
-    const currentMovieIndex = myWatchlist.indexOf(...currentMovie)
-    if (currentMovieIndex === 0) return
-
-    setMyWatchlist((prevWatchlist) => [
-      ...prevWatchlist.slice(0, currentMovieIndex - 1),
-      ...currentMovie,
-      ...prevWatchlist.slice(currentMovieIndex - 1, currentMovieIndex),
-      ...prevWatchlist.slice(currentMovieIndex + 1),
-    ])
-  }
-
-  function reorderDown(id) {
-    const currentMovie = myWatchlist.filter((movie) => movie.cardId === id)
-    const currentMovieIndex = myWatchlist.indexOf(...currentMovie)
-    if (currentMovieIndex === myWatchlist.length - 1) return
-
-    setMyWatchlist((prevWatchlist) => [
-      ...prevWatchlist.slice(0, currentMovieIndex),
-      ...prevWatchlist.slice(currentMovieIndex + 1, currentMovieIndex + 2),
-      ...currentMovie,
-      ...prevWatchlist.slice(currentMovieIndex + 2),
-    ])
-  }
-
-  function toggleDarkMode() {
-    setDarkMode((prevDarkMode) => !prevDarkMode)
-  }
-
+  // ***** CHANGE BODY COLOR ON DARKMODE CHANGE *****
   useEffect(() => {
     if (darkMode) {
       document.body.style.backgroundColor = 'var(--bg-dark)'
     } else document.body.style.backgroundColor = 'var(--bg-light)'
   }, [darkMode])
 
+  // ***** TOGGLE DARKMODE *****
+  function toggleDarkMode() {
+    setDarkMode((prevDarkMode) => !prevDarkMode)
+  }
+
+  // ***** TOGGLE WATCHLIST / SEARCH PAGE *****
   function toggleWatchlist() {
     setShowWatchlist((prevState) => !prevState)
   }
 
+  // ***** GET WATCHLIST FROM LOCAL STORAGE *****
+  useEffect(() => {
+    if (localStorage.getItem('myWatchlist')) {
+      setMyWatchlist(JSON.parse(localStorage.getItem('myWatchlist')))
+    } else return
+  }, [])
+  // ***** UPDATE WATCHLIST IN LOCAL STORAGE ANYTIME IT CHANGES *****
+  useEffect(() => {
+    localStorage.setItem('myWatchlist', JSON.stringify(myWatchlist))
+  }, [myWatchlist])
+
+  // ***** ADD MOVIE TO WATCHLIST HANDLER *****
   function addToWatchlist(poster, title, rating, runtime, genre, plot, cardId) {
+    if (!myWatchlist.every((movie) => movie.cardId !== cardId)) {
+      setAlert(true)
+      setTimeout(() => {
+        setAlert(false)
+      }, 1000)
+      return
+    }
     setMyWatchlist((prevWatchlist) => [
       ...prevWatchlist,
       {
@@ -68,17 +66,42 @@ function App() {
     ])
   }
 
-  // localStorage.clear()
-  useEffect(() => {
-    if (localStorage.getItem('myWatchlist')) {
-      setMyWatchlist(JSON.parse(localStorage.getItem('myWatchlist')))
-    } else return
-  }, [])
+  // ***** REMOVE MOVIE FROM WATCHLIST HANDLER *****
+  function removeFromWatchlist(poster) {
+    setMyWatchlist((prevWatchlist) => {
+      return prevWatchlist.filter((movie) => movie.poster !== poster)
+    })
+  }
 
-  useEffect(() => {
-    localStorage.setItem('myWatchlist', JSON.stringify(myWatchlist))
-  }, [myWatchlist])
+  // ***** REORDERING WATCHLIST HANDLERS - MOVE UP *****
+  function reorderUp(id) {
+    const currentMovie = myWatchlist.filter((movie) => movie.cardId === id)
+    const currentMovieIndex = myWatchlist.indexOf(...currentMovie)
+    if (currentMovieIndex === 0) return
 
+    setMyWatchlist((prevWatchlist) => [
+      ...prevWatchlist.slice(0, currentMovieIndex - 1),
+      ...currentMovie,
+      ...prevWatchlist.slice(currentMovieIndex - 1, currentMovieIndex),
+      ...prevWatchlist.slice(currentMovieIndex + 1),
+    ])
+  }
+
+  // ***** REORDERING WATCHLIST HANDLERS - MOVE DOWN *****
+  function reorderDown(id) {
+    const currentMovie = myWatchlist.filter((movie) => movie.cardId === id)
+    const currentMovieIndex = myWatchlist.indexOf(...currentMovie)
+    if (currentMovieIndex === myWatchlist.length - 1) return
+
+    setMyWatchlist((prevWatchlist) => [
+      ...prevWatchlist.slice(0, currentMovieIndex),
+      ...prevWatchlist.slice(currentMovieIndex + 1, currentMovieIndex + 2),
+      ...currentMovie,
+      ...prevWatchlist.slice(currentMovieIndex + 2),
+    ])
+  }
+
+  // ***** DARKMODE TOGGLER ELEMENT *****
   const togglerEl = (
     <div className="toggler-div">
       <label className="switch">
@@ -88,11 +111,7 @@ function App() {
     </div>
   )
 
-  function removeFromWatchlist(poster) {
-    setMyWatchlist((prevWatchlist) => {
-      return prevWatchlist.filter((movie) => movie.poster !== poster)
-    })
-  }
+  const duplicateMovieAlert = <h5 className="popup fade-out">Move already on watchlist!</h5>
 
   return (
     // <main style={bgStyle}>
@@ -119,6 +138,7 @@ function App() {
           showWatchlist={showWatchlist}
         />
       )}
+      {alert && duplicateMovieAlert}
     </main>
   )
 }
